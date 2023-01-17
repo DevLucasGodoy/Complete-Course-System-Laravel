b3bc14304be041b93745df4ee27b67b2dd4ae060
Lucas Godoy <54687280+DevLucasGodoy@users.noreply.github.com>
2023-01-16 22:33:39 -0400

    first commit

---
+++ resources/views/dashboard/index.blade.php
@@ -0,0 +1,22 @@
+@extends('dashboard.layouts.maintemplate')
+
+@section('title', 'Dashboard')
+
+@section('content')
+    <!-- Begin Page Content -->
+    <div class="container-fluid">
+        <!-- Page Heading -->
+        <div class="d-sm-flex align-items-center justify-content-between mb-4">
+            <h1 class="h3 mb-0 text-gray-800">Seja Muito Bem-Vindo(a)</h1>
+        </div>
+    </div>
+
+    <div class="container">
+        <div class="card text-center pt-3">
+            <div class="card-body">
+                <h5 class="card-title display-4 text-info"><b>Beyond Borders</b></h5>
+            </div>
+            
+        </div>
+    </div>
+@endsection
